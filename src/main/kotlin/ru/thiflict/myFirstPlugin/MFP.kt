a360d8b74cdf9ee5cdd48b2ca724ea6bf3618cee
Thiflict <121az348@gmail.com>
2024-11-16 08:47:03 +0500

    v0.0.0 Born

---
+++ src/main/kotlin/ru/thiflict/myFirstPlugin/MFP.kt
@@ -0,0 +1,14 @@
+package ru.thiflict.myFirstPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MFP : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
